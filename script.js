6529d18656d1881e92b71fc4d6eb8968ef4613f9
JooCode | Programmer <joomodz5@gmail.com>
2025-03-04 20:39:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-// Efek Mengetik
-const text = "Jose Programmer";
-let index = 0;
-
-function typeEffect() {
-    if (index < text.length) {
-        document.getElementById("typing-text").innerHTML += text.charAt(index);
-        index++;
-        setTimeout(typeEffect, 100);
-    }
-}
-
-window.onload = typeEffect;
-
-// Scroll ke Bagian Video
 function scrollToSection(sectionId) {
     document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
+}
+
+function playAudio() {
+    let audio = document.getElementById("bg-audio");
+    audio.play();
 }
